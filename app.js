3793d57a990db4c29f551acece5060128d43d07e
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2023-06-09 02:21:02 +0500

    HGDSJ

--- app.js
+++ app.js
@@ -1,39 +1,89 @@
 
+
+function LogingetValue(){
+
+
+    var loginemail = document.getElementById("loginEmail").value
+    var loginPassword = document.getElementById("loginPass").value
+    var Email = localStorage.getItem("SignupEmail")
+    var Password = localStorage.getItem("ConfirmPassword")
+ 
+      if(loginemail.trim() == "" && loginPassword.trim() == ""){
+        
+        Swal.fire({
+            icon: 'error',
+            title: 'error',
+            text: 'Input Field Must be filled',
+            footer: '<a href="">Why do I have this issue?</a>'
+          })
+        }else{
+    
+            if( loginPassword !== Password && loginemail !== Email ){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'error',
+                    text: 'Invalid email or password',
+                    footer: '<a href="">Why do I have this issue?</a>'
+                  })
+            }else{
+                Swal.fire({
+                    icon: 'success',
+                    title: 'You Login Succefully!',
+                    
+              
+                })
+            }
+        }
+        
+}
 function SignUpgetValue(){
 
     var signUpEmail = document.getElementById("signUpEmail").value
     var signUppassword = document.getElementById("signUpPass").value
-   
+    var fname = document.getElementById("fname").value
+    var lname = document.getElementById("lname").value
+    var signUpconPass = document.getElementById("signUpconPass").value
+    
+
+    localStorage.setItem("FirstName",fname)
+    localStorage.setItem("LastName",lname)
     localStorage.setItem("SignupEmail",signUpEmail)
-    localStorage.setItem("Signuppassword",signUppassword)
-
-}
-var localStorageSignUpEmail = localStorage.getItem("SignupEmail")
-var localStorageSignUpPassword = localStorage.getItem("Signuppassword")
-
-
-function LogingetValue(){
-
-    var loginEmail = document.getElementById("loginEmail").value
-    var loginPassword = document.getElementById("loginPass").value
-   
-    localStorage.setItem("LoginEmail",loginEmail)
-    localStorage.setItem("LoginPassword",loginPassword)
-}
-
-var localStorageLoginEmail = localStorage.getItem("LoginEmail")
-var localStorageLoginPassword = localStorage.getItem("LoginPassword")
-
-
+    localStorage.setItem("ConfirmPassword",signUpconPass)
 
     
-if(localStorageSignUpEmail === localStorageLoginEmail){
+    if( signUpEmail.trim() == "" && signUppassword.trim()=="" && fname.trim()=="" && lname.trim() =="" && signUpconPass.trim()==""){
 
-    if(localStorageLoginPassword === localStorageSignUpPassword){
-        swal("Login SuccessFul", "You clicked the button!", "success")
+        Swal.fire({
+            icon: 'error',
+            title: 'error',
+            text: 'Input Field Must be filled',
+            footer: '<a href="">Why do I have this issue?</a>'
+          })
+
+    }else{
+    
+        if(signUppassword !== signUpconPass){
+            Swal.fire({
+                icon: 'error',
+                title: 'error',
+                text: 'Password is not matching',
+                footer: '<a href="">Why do I have this issue?</a>'
+              })
+        }else{
+            Swal.fire({
+                icon: 'success',
+                title: 'You SignUp Succefully!',
+                
+          
+            })
+        }
     }
-}else if(localStorageSignUpEmail !== localStorageLoginEmail){
-    sweetAlert("Login Failed", "Please Enter Correct Email", "error");  
-      }
-    
+
+}
+
+
+                
+                
+
+
 
